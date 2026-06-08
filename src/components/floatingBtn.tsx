cef4e59cb7fa1cc96f11112bfbf7d49015569e92
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-06-08 14:53:48 +0530

    floating btn

--- src/components/floatingBtn.tsx
+++ src/components/floatingBtn.tsx
@@ -6,7 +6,7 @@ export default function WhatsAppButton() {
       href="https://wa.me/+917230941008"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed md:bottom-10 right-4 md:mb-11 md:w-14 md:h-14 w-12 h-12 bottom-0 mb-5 bg-green-500 rounded-full flex items-center justify-center shadow-lg z-50"
+      className="fixed md:bottom-5 right-4 md:mb-5 md:w-14 md:h-14 w-12 h-12 bottom-0 mb-5 bg-green-500 rounded-full flex items-center justify-center shadow-lg z-50"
     >
       <MessageCircle className="text-white" size={21} />
     </a>
