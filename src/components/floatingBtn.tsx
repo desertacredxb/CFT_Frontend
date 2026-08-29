62ea6730c57c65a698c585476e699a4cfa9d4aae
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-08-29 12:13:27 +0530

    mobile whatsappp overlap ui

--- src/components/floatingBtn.tsx
+++ src/components/floatingBtn.tsx
@@ -7,7 +7,7 @@ export default function WhatsAppButton() {
       href="https://whatsapp.com/channel/0029VaMrKNc6rsQvPQFC863Z"
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed md:bottom-16 right-6 md:w-14 md:h-14 w-12 h-12 bottom-6 mb-8 bg-green-500 rounded-full flex items-center justify-center shadow-lg z-50"
+      className="fixed bottom-16 right-6 w-12 h-12 mb-8 md:w-14 md:h-14 bg-green-500 rounded-full flex items-center justify-center shadow-lg z-50"
     >
       <MessageCircle className="text-white" size={21} />
     </a>
