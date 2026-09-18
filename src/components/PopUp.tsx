4d43271f6ee4ed6e812e2c01a7f5e626437895e1
Anshu Maurya <anshuweb@bigwigmediadigital.com>
2026-09-18 18:10:35 +0530

    updated deployement bug

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -3,8 +3,6 @@ import { useNavigate } from "react-router-dom";
 import "../index.css";
 import { usePopup } from "../components/PopupContext";
 import { toast } from "react-toastify";
-const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://cft-backend.onrender.com";
 
 const Popup = () => {
   const navigate = useNavigate();
@@ -90,25 +88,6 @@ const Popup = () => {
         referralcode: hasReferralCode ? referralCode : "",
       };
 
-      // Prepare requests to run in parallel
-      // const thirdPartyPromise = fetch("/api/apiUserRegister", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //     "x-api-key":
-      //       import.meta.env.VITE_MT_AUTH_KEY ||
-      //       "X9dPa4Lm7QvR2nHt8YsK5cZw1FuJ6eGb",
-      //   },
-      //   body: JSON.stringify(payload),
-      // });
-
-      // const localLeadPromise = fetch("/api/leads", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //   },
-      //   body: JSON.stringify(payload),
-      // });
 
       // 1. Third Party API (Needs full URL in production)
       const thirdPartyPromise = fetch("/api/leads/register-user", {
